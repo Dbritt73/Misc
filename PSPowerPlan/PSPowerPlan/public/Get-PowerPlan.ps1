e4ac8ff4745a8686d799223f3c40dc1ef3dcc8a6
Darrin Britton <brittod@wwu.edu>
2018-12-11 09:13:01 -0800

    added errorhandling and template for comment help

--- PSPowerPlan/PSPowerPlan/public/Get-PowerPlan.ps1
+++ PSPowerPlan/PSPowerPlan/public/Get-PowerPlan.ps1
@@ -1,8 +1,40 @@
 Function Get-PowerPlan {
+  <#
+    .SYNOPSIS
+    Describe purpose of "Get-PowerPlan" in 1-2 sentences.
+
+    .DESCRIPTION
+    Add a more complete description of what the function does.
+
+    .PARAMETER ComputerName
+    Describe parameter -ComputerName.
+
+    .EXAMPLE
+    Get-PowerPlan -ComputerName Value
+    Describe what this call does
+
+    .NOTES
+    Place additional notes here.
+
+    .LINK
+    URLs to related sites
+    The first link is opened by Get-Help -Online Get-PowerPlan
+
+    .INPUTS
+    List of input types that are accepted by this function.
+
+    .OUTPUTS
+    List of output types produced by this function.
+  #>
+
+
     [CmdletBinding()]
     Param (
 
-        [string[]]$ComputerName
+        [Parameter( ValueFromPipeline = $True,
+                    ValueFromPipelineByPropertyName = $True,
+                    Position = 0)]
+        [string[]]$ComputerName = 'localhost'
 
     )
 
@@ -46,7 +78,27 @@ Function Get-PowerPlan {
 
                         Write-output -InputObject $obj
 
-            } Catch {}
+            } Catch {
+
+                # get error record
+                [Management.Automation.ErrorRecord]$e = $_
+
+                # retrieve information about runtime error
+                $info = [PSCustomObject]@{
+
+                  Exception = $e.Exception.Message
+                  Reason    = $e.CategoryInfo.Reason
+                  Target    = $e.CategoryInfo.TargetName
+                  Script    = $e.InvocationInfo.ScriptName
+                  Line      = $e.InvocationInfo.ScriptLineNumber
+                  Column    = $e.InvocationInfo.OffsetInLine
+
+                }
+
+                # output information. Post-process collected info, and log info (optional)
+                $info
+
+            }
 
         }
 
