9bede91d9776e3dc24391a03fa25f37b641b779f
Darrin Britton <brittod@wwu.edu>
2019-02-06 08:43:27 -0800

    added set-hypervpermissions

---
+++ Set-HyperVPermissions.ps1
@@ -0,0 +1,31 @@
+Function Set-HyperVPermissions {
+    [CmdletBinding()]
+    Param (
+        [String[]]$ComputerName
+    )
+
+    Begin {}
+
+    Process {
+
+        Foreach ($Computer in $ComputerName) {
+
+            Try {
+
+                Invoke-Command -ComputerName $Computer -ScriptBlock {
+
+                    Add-LocalGroupMember -Group 'Hyper-V Administrators' -Member 'NT Virtual Machine\Virtual Machines'
+
+                    Add-LocalGroupMember -Group 'Remote Management Users' -Member 'NT Virtual Machine\Virtual Machines'
+
+                }
+
+            } Catch {}
+
+        }
+
+    }
+
+    End {}
+
+}
